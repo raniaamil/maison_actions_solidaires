cfa4f92b64c42b61a9e0a926ea15053cde81a521
Rania Amil <mademoiselleamil@gmail.com>
2025-08-17 23:04:41 +0200

    page association complète. Plus rien à modifier.

--- src/app/association/page.tsx
+++ src/app/association/page.tsx
@@ -88,66 +88,122 @@ const MaisonActionsSolidaires = () => {
                 <p>
                   "Permettez-moi de partager avec vous un peu de mon histoire. Issue d'une fratrie de 18 enfants, 
                   j'ai grandi dans un environnement où la solidarité, l'entraide et le partage étaient des valeurs 
-                  fondamentales. Ces principes m'ont accompagnée tout au long de ma vie."
+                  fondamentales. Ces principes m'ont accompagnée tout au long de ma vie.
                 </p>
                 <p>
-                  "Depuis 2015, lors de mes différentes missions en tant qu'aide-soignante dans des maisons d'accueil 
+                  Depuis 2015, lors de mes différentes missions en tant qu'aide-soignante dans des maisons d'accueil 
                   spécialisées, des maisons de retraite, des services de soins à domicile, ou encore des centres de 
                   rééducation, j'ai eu l'occasion de prendre soin des personnes âgées, des personnes en situation de 
-                  handicap, ainsi que des aidants."
+                  handicap, ainsi que des aidants.
                 </p>
                 <p>
-                  "Ces expériences m'ont ouvert les yeux sur les besoins criants de ces publics et m'ont amenée à 
+                  Ces expériences m'ont ouvert les yeux sur les besoins criants de ces publics et m'ont amenée à 
                   réfléchir à des solutions pour leur venir en aide. C'est au cours de cette réflexion que j'ai eu 
-                  le déclic de créer une association."
+                  le déclic de créer une association.
                 </p>
                 <p>
-                  "Ce déclic s'est concrétisé grâce à mon intégration à la MDPH (Maison Départementale des Personnes 
+                  Ce déclic s'est concrétisé grâce à mon intégration à la MDPH (Maison Départementale des Personnes 
                   Handicapées). En écoutant les doléances des usagers et lors d'un séminaire de la MDPH, j'ai fait une 
                   rencontre décisive : la directrice de l'association Solidaire Internationale. Elle m'a rappelé 
-                  l'importance de ne pas repousser ses projets."
+                  l'importance de ne pas repousser ses projets.
                 </p>
                 <p>
-                  "Ce conseil a marqué un tournant dans ma vie, me convainquant qu'il était temps de me lancer. J'ai 
+                  Ce conseil a marqué un tournant dans ma vie, me convainquant qu'il était temps de me lancer. J'ai 
                   ensuite pris contact avec d'autres personnes partageant cet esprit associatif. Leur soutien et leur 
-                  engagement m'ont permis de concrétiser ce projet."
+                  engagement m'ont permis de concrétiser ce projet.
                 </p>
                 <p>
-                  "En un an seulement, avec une équipe dynamique et motivée, nous avons accompli de belles choses. 
-                  Et nous espérons faire encore mieux en 2025, notamment grâce à votre précieux soutien. Ensemble, 
+                  En un an seulement, avec une équipe dynamique et motivée, nous avons accompli de belles choses. 
+                  Et nous espérons faire encore mieux, notamment grâce à votre précieux soutien. Ensemble, 
                   nous avons accompli tant de choses, et je suis impatiente de voir ce que nous réaliserons à l'avenir."
                 </p>
               </div>
             </div>
 
-            {/* Marie ROUSSEAU */}
+            {/* Kimberley */}
             <div className={styles.teamCard}>
               <div className={styles.teamAvatar}>
-                <span className={styles.teamInitials}>MR</span>
+                <Image
+                  src="/images/association/photo_kimberley.jpg"
+                  alt="Kimberley"
+                  width={96}
+                  height={96}
+                  className={styles.teamPhoto}
+                />
               </div>
-              <h4 className={styles.teamName}>Marie ROUSSEAU</h4>
-              <p className={styles.teamRole}>Responsable Pôle Numérique</p>
-              <p className={styles.teamBio}>
-                Formatrice expérimentée en informatique, Marie anime nos 
-                ateliers de formation numérique et accompagne les bénéficiaires vers 
-                l'autonomie digitale. Elle développe des méthodes pédagogiques 
-                adaptées à chacun.
-              </p>
+              <h4 className={styles.teamName}>Kimberley</h4>
+              <p className={styles.teamRole}>Responsable Pôle Administratif</p>
+              <div className={styles.teamBioExtended}>
+                <p>
+                  "Bonjour je suis.... Je suis... KIMBERLEY !
+                </p>
+                <p>
+                  Vous avez la trouille de faire des CV, lettres de motivation, relances, lettres à la CAF...? 
+                </p>
+                <p>
+                  Vous ne savez pas envoyer des mails ? Google vous fait peur ? Pas de soucis, nous sommes là pour vous aider.
+                </p>
+                <p>
+                  Je travaille en tant que commerciale, donc Word, Google, Outlook fait parti de mon quotidien. 
+                  Au plaisir de vous rencontrer !"
+                </p>
+              </div>
             </div>
 
-            {/* Jean DUBOIS */}
+            {/* Stéphane AURIAULT */}
             <div className={styles.teamCard}>
               <div className={styles.teamAvatar}>
-                <span className={styles.teamInitials}>JD</span>
+                <Image
+                  src="/images/association/photo_stéphane.jpg"
+                  alt="Stéphane AURIAULT"
+                  width={96}
+                  height={96}
+                  className={styles.teamPhoto}
+                />
               </div>
-              <h4 className={styles.teamName}>Jean DUBOIS</h4>
-              <p className={styles.teamRole}>Psychologue & Coordinateur Pôle Soutien</p>
-              <p className={styles.teamBio}>
-                Psychologue clinicien spécialisé dans l'accompagnement des 
-                aidants, Jean anime nos groupes de parole et assure le suivi 
-                psychologique individuel. Il coordonne notre plateforme 
-                d'écoute téléphonique.
-              </p>
+              <h4 className={styles.teamName}>Stéphane AURIAULT</h4>
+              <p className={styles.teamRole}>Praticien de Bien-être - Pôle Bien-être</p>
+              <div className={styles.teamBioExtended}>
+                <p>
+                  "Bonjour, je suis Stéphane Auriault, je suis praticien de bien-être.
+                </p>
+                <p>
+                  J'exerce la Podoréflexologie appliquée à la Médecine Traditionnelle Chinoise, la Réflexologie palmaire 
+                  avec des conseils de Podoréflexologie pour amoindrir voire quelquefois faire disparaître les douleurs, 
+                  la réflexologie plantaire couplée parfois au shiatsu de bien-être pour traiter les émotions.
+                </p>
+                <p>
+                  Je pratique également le guasha-ventouses, guasha-moxa et moxa-ventouses, la réflexologie plantaire 
+                  avec la moxibution. Je suis également certifié pour donner des conseils de base en naturopathie."
+                </p>
+              </div>
+            </div>
+
+            {/* Autres membres de l'équipe */}
+            <div className={styles.teamCard}>
+              <div className={styles.teamAvatar}>
+                <span className={styles.teamInitials}>👥</span>
+              </div>
+              <h4 className={styles.teamName}>Autres Membres</h4>
+              <p className={styles.teamRole}>Équipe Administrative & Pédagogique</p>
+              <div className={styles.teamBio}>
+                <div className={styles.teamMemberItem}>
+                  <strong>Audrey KAGO</strong>
+                  <br />
+                  <span className={styles.memberRole}>Secrétaire</span>
+                </div>
+                <div className={styles.teamMemberItem}>
+                  <strong>Hippolyte NGOUAJO</strong>
+                  <br />
+                  <span className={styles.memberRole}>Trésorier</span>
+                </div>
+                <div className={styles.teamMemberItem}>
+                  <strong>Cataracte SMARTINS</strong>
+                  <br />
+                  <span className={styles.memberRole}>AESH/Enseignante - Pôle Junior</span>
+                  <span className={styles.memberDescription}>Accompagnante des enfants en situation de handicap par des ateliers récréatifs</span>
+                </div>
+              </div>
             </div>
           </div>
 
